8f8d764690934649e75a169504350f1b80d6d8ee
yuji0223 <ok.yuji.0223@gmail.com>
2020-07-08 17:31:13 +0900

    Cardモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, length: { maximum: 20 }
-
   has_many :lists, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 20 }
 end
