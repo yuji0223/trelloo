bcde639fe6121d549cd7ac554f115f26d0f923ef
yuji0223 <ok.yuji.0223@gmail.com>
2020-07-08 14:16:06 +0900

    リスト編集機能

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,4 +1,5 @@
 class ListController < ApplicationController
+  before_action :set_list, only: [:edit, :update]
   def new
     @list = List.new
   end
@@ -12,8 +13,23 @@ class ListController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @list.update_attributes(list_params)
+      redirect_to :root
+    else
+      render action :edit
+    end
+  end
+
   private
   def list_params
     params.require(:list).permit(:title).merge(user: current_user)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
