cad296b88cc42b15373d9cf4ad76952a4ecfe330
yuji0223 <ok.yuji.0223@gmail.com>
2020-07-11 15:18:46 +0900

    カード順番変更機能

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -8,7 +8,7 @@ class CardController < ApplicationController
 
   def create
     @card = Card.new(card_params)
-    @list = List.find(params[:list_id])
+    @card.order = Card.where(list_id: params[:list_id]).maximum(:order).to_i + 1
     if @card.save
       redirect_to :root
     else
@@ -21,10 +21,28 @@ class CardController < ApplicationController
 
   def edit
     @lists = List.where(user: current_user)
+    @cards = Card.where(list_id: params[:list_id]).order(:order)
   end
 
   def update
-    @lists = List.where(user: current_user)
+    if params[:list_id] != card_params[:list_id]
+      @card.order = Card.where(list_id: card_params[:list_id]).maximum(:order).to_i + 1
+    elsif @card.order != card_params[:order]
+      order_after = card_params[:order].to_i
+      if @card.order > order_after
+        (order_after..@card.order-1).each {|n|
+          @card_order = Card.find_by(list_id: params[:list_id], order: n)
+          @card_order.update_attributes(order: n+1)
+        }
+      elsif @card.order < order_after
+        (@card.order+1..order_after).each {|n| 
+          @card_order = Card.find_by(list_id: params[:list_id], order: n)
+          @card_order.update_attributes(order: n-1)
+        }
+      end
+      @card.order = card_params[:order]
+    end
+
     if @card.update_attributes(card_params)
       redirect_to :root
     else
@@ -33,13 +51,17 @@ class CardController < ApplicationController
   end
 
   def destroy
+    cards = Card.where(list_id: params[:list_id], order: @card.order+1..Float::INFINITY)
+    cards.each { |card|
+      card.update_attributes(order: card.order-1)
+    }
     @card.destroy
     redirect_to :root
   end
 
   private
   def card_params
-    params.require(:card).permit(:title, :memo, :list_id)
+    params.require(:card).permit(:title, :memo, :list_id, :order)
   end
 
   def set_card
